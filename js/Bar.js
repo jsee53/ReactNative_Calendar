c285a74c0ee815914d0bdd8bccd9582ff0da1c57
SeeYoung Jin <jsee53@naver.com>
2023-05-23 17:21:58 +0900

    5-23(2)

--- js/Bar.js
+++ js/Bar.js
@@ -1,17 +1,56 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
 const Bar = () => {
   const today = new Date();
+
+  const [selectedYear, setSelectedYear] = useState(today.getFullYear());
+  const [selectedMonth, setSelectedMonth] = useState(today.getMonth() + 1);
+
+  const handleYearPress = (year) => {
+    setSelectedYear(year);
+  };
+
+  const handleMonthPress = (month) => {
+    setSelectedMonth(month);
+  };
+
+  const options = {
+    years: [2021, 2022, 2023],
+    months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.leftContainer}>
         <Image source={require("../favicon/profile.png")} style={styles.icon} />
-        <Text>
-          {today.getFullYear()}년 {today.getMonth() + 1}월
-        </Text>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <Text style={styles.label}>
+            {selectedYear}년 {selectedMonth}월
+          </Text>
+
+          {options.years.map((year) => (
+            <TouchableOpacity
+              key={year}
+              onPress={() => handleYearPress(year)}
+            ></TouchableOpacity>
+          ))}
+          {options.months.map((month) => (
+            <TouchableOpacity
+              key={month}
+              onPress={() => handleMonthPress(month)}
+            ></TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
+      <View style={styles.centerContainer} />
       <View style={styles.rightContainer}>
         <Image source={require("../favicon/search.png")} style={styles.icon} />
         <Image source={require("../favicon/alert.png")} style={styles.icon} />
@@ -22,20 +61,31 @@ const Bar = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row", // 아이템을 수평 방향으로 정렬
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between", // 왼쪽과 오른쪽 사이 공간을 동일하게 분할
-    paddingHorizontal: 16, // 좌우 여백
-    height: 60, // 바의 높이
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    height: 60,
+    width: 300,
     backgroundColor: "#f5f5f5",
   },
   leftContainer: {
-    flexDirection: "row", // 왼쪽 아이템들을 수평 방향으로 정렬
+    flexDirection: "row",
     alignItems: "center",
   },
+  centerContainer: {
+    flex: 1,
+  },
   rightContainer: {
-    flexDirection: "row", // 오른쪽 아이템들을 수평 방향으로 정렬
+    flexDirection: "row",
     alignItems: "center",
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+  text: {
+    marginLeft: 8,
   },
 });
 
